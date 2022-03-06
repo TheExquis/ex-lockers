ca100e6cedde4f572c433e60556d5649f40e330c
tony-stark-17 <76168122+tony-stark-17@users.noreply.github.com>
2022-03-06 17:59:14 +0530

    Configuration Option for drawtext and Fivem-Target

--- config.lua
+++ config.lua
@@ -4,3 +4,7 @@ Config.LockerZone = {
     Legion2 = vector4(-2031.3180, -464.9169, 10.6040, 232.0638),
     Legion = vector4(-275.2015, -917.0491, 30.2163, 74.5762),
 }
+
+Config.DrawText = true -- SHOULD BE FALSE IF TARGET IS TRUE
+
+Config.Target = false -- SHOULD BE FALSE IF DrawText IS TRUE
