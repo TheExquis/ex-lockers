177b9bb4ac6266e9e4141864259bdf92b947a756
Tony Stark <76168122+tony-stark-17@users.noreply.github.com>
2022-03-10 23:59:30 +0530

    Update SQL

--- tslocker.sql
+++ tslocker.sql
@@ -1,8 +1,12 @@
-
-CREATE TABLE IF NOT EXISTS `tslockers` (
-  `lockerid` int(20) NOT NULL AUTO_INCREMENT,
-  `owner` varchar(255) NOT NULL,
-  `password` int(11) DEFAULT 0,
-  `branch` varchar(50) NOT NULL,
-  KEY `lockerid` (`lockerid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `tslockers` (
+	`dbid` INT(20) NOT NULL AUTO_INCREMENT,
+	`lockerid` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`owner` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`password` VARCHAR(20) NOT NULL DEFAULT '123' COLLATE 'utf8mb4_general_ci',
+	`branch` VARCHAR(20) NOT NULL COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`dbid`) USING BTREE,
+	INDEX `lockerid` (`lockerid`) USING BTREE
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+;
