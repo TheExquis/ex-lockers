36a42a4ae22af187796a4570e1cd5ac5862625eb
Tony Stark <76168122+tony-stark-17@users.noreply.github.com>
2022-09-03 13:10:05 +0530

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,8 +1,8 @@
 Config = {}
 
 Config.LockerZone = {
-    Legion2 = vector4(-2031.3180, -464.9169, 10.6040, 232.0638),
-    Legion = vector4(-275.2015, -917.0491, 30.2163, 74.5762),
+    Legion2 = vector3(-2031.3180, -464.9169, 10.6040),
+    Legion = vector3(-275.2015, -917.0491, 30.2163),
 }
 
 Config.DrawText = true -- SHOULD BE FALSE IF TARGET IS TRUE
