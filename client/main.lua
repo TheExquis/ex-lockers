9c1e1924cda4dbbc2b5943948aea9bd98798e17c
Tony Stark <76168122+tony-stark-17@users.noreply.github.com>
2022-09-05 15:12:23 +0530

    Bug Fixes and optimization

--- client/main.lua
+++ client/main.lua
@@ -1,39 +1,24 @@
 local ESX = exports['es_extended']:getSharedObject()
 
-Citizen.CreateThread(function()
-    local sleep = 3000
-    local inZone = false
-    while true do
-        local nearArea = false
-        local coords = GetEntityCoords(PlayerPedId())
-        for k, v in pairs(Config.LockerZone) do
-            local dist = #(v - coords)
-            if dist < 5 then
-                nearArea = true
-                DisableControlAction(0, 47)
-                if IsDisabledControlJustPressed(0, 47) then
-                    ESX.TriggerServerCallback("ts-lockers:getLockers", function(data) 
-                        TriggerServerEvent('ts-lockers:LoadStashes')
-                        TriggerEvent("ts-lockers:OpenMenu", {locker = k, info = data})
-                    end, k)
-                    
-                end
-                DrawText3Ds(v.x,v.y,v.z+1.0, "Press ~r~[G]~s~ To Open ~y~Locker~s~")
+CreateThread(function()
+    for k, v in pairs(Config.LockerZone) do
+        v.point = lib.points.new(v.coords, 5)
+        function v.point:nearby()
+            DrawText3Ds(v.coords.x, v.coords.y, v.coords.z + 1.0, "Press ~r~[G]~s~ To Open ~y~Locker~s~")
+            DisableControlAction(0, 47)
+            if IsDisabledControlJustPressed(0, 47) then
+                ESX.TriggerServerCallback("ts-lockers:getLockers", function(data)
+                    TriggerServerEvent('ts-lockers:LoadStashes')
+                    TriggerEvent("ts-lockers:OpenMenu", { locker = k, info = data })
+                end, k)
             end
         end
-        if nearArea and not inZone then
-            inZone = true
-            sleep = 0
-        end
-        if not nearArea and inZone then           
-            sleep = 3000
-        end
-        Citizen.Wait(sleep)
     end
 end)
 
+
 RegisterNetEvent("ts-lockers:OpenMenu", function(data)
-	lib.registerContext({
+    lib.registerContext({
         id = 'locker_menu',
         title = 'EX Lockers',
         options = {
@@ -45,7 +30,7 @@ RegisterNetEvent("ts-lockers:OpenMenu", function(data)
                     branch = data.locker
                 }
             },
-			['Open Locker'] = {
+            ['Open Locker'] = {
                 description = 'Open Existing Locker',
                 arrow = true,
                 event = 'ts-lockers:LockerList',
@@ -54,7 +39,7 @@ RegisterNetEvent("ts-lockers:OpenMenu", function(data)
                     branch = data.locker
                 }
             },
-			['Open Your Locker'] = {
+            ['Open Your Locker'] = {
                 description = 'Open Self Locker',
                 arrow = true,
                 event = 'ts-lockers:OpenSelfLocker',
@@ -63,7 +48,7 @@ RegisterNetEvent("ts-lockers:OpenMenu", function(data)
                     branch = data.locker
                 }
             },
-			['Delete Locker'] = {
+            ['Delete Locker'] = {
                 description = 'Delete Existing Locker',
                 arrow = true,
                 event = 'ts-lockers:LockerListDelete',
@@ -72,7 +57,7 @@ RegisterNetEvent("ts-lockers:OpenMenu", function(data)
                     branch = data.locker
                 }
             },
-			['Change Locker Password'] = {
+            ['Change Locker Password'] = {
                 description = 'Change Existing Locker Password',
                 arrow = true,
                 event = 'ts-lockers:LockerChangePass',
@@ -82,70 +67,89 @@ RegisterNetEvent("ts-lockers:OpenMenu", function(data)
                 }
             }
         }
-	})
+    })
     lib.showContext('locker_menu')
 end)
 
 RegisterNetEvent('ts-lockers:LockerList', function(data)
-	local optionTable = {}
-        local arg = data.arg
-	local idt = 2
-    	if arg then
-    		for k,v in pairs(arg) do
-        		idt = idt + 1
-			optionTable["Locker ID: "..v.dbid] = {
-				description = 'Owner: '..v.playername,
-                		arrow = true,
-                		event = 'ts-lockers:client:OpenLocker',
-                		args = v
-			}
-    		end
-    	end
-	lib.registerContext({
+    local optionTable = {}
+    local arg = data.arg
+    local idt = 2
+    if arg then
+        for k, v in pairs(arg) do
+            idt = idt + 1
+            optionTable["Locker ID: " .. v.dbid] = {
+                description = 'Owner: ' .. v.playername,
+                arrow = true,
+                event = 'ts-lockers:client:OpenLocker',
+                args = v
+            }
+        end
+    end
+    lib.registerContext({
         id = 'locker_list',
-        title = data.branch..' Locker Menu',
-		menu = "locker_menu",
+        title = data.branch .. ' Locker Menu',
+        menu = "locker_menu",
         options = optionTable
-	})
-    	lib.showContext('locker_list')
+    })
+    lib.showContext('locker_list')
 end)
 
 RegisterNetEvent('ts-lockers:LockerChangePass', function(data)
-	local Ply = ESX.GetPlayerData()
-    	local lockers = data.arg
-    	for k,v in pairs(lockers) do
-        	if Ply.identifier == v.owner then
-            		TriggerEvent('ts-lockers:client:ChangePassword', {data = v})
-        	end
-    	end
+    local Ply = ESX.GetPlayerData()
+    local lockers = data.arg
+    if lockers then
+        local exist = false
+        for k, v in pairs(lockers) do
+            if Ply.identifier == v.owner then
+                exist = true
+                TriggerEvent('ts-lockers:client:ChangePassword', { data = v })
+            end
+        end
+        if not exist then
+            ESX.ShowNotification("You don't have a locker")
+        end
+    else
+        ESX.ShowNotification("You don't have a locker")
+    end
 end)
 
 RegisterNetEvent('ts-lockers:LockerListDelete', function(data)
-	local Ply = ESX.GetPlayerData()
-    	local lockers = data.arg
-    	for k,v in pairs(lockers) do
-        	if Ply.identifier == v.owner then
-            		TriggerEvent('ts-lockers:client:DeleteLocker', {data = v,id = v.lockerid})
-        	end
-    	end
+    local Ply = ESX.GetPlayerData()
+    local lockers = data.arg
+    if lockers then
+        local exist = false
+        for k, v in pairs(lockers) do
+            if Ply.identifier == v.owner then
+                exist = true
+                TriggerEvent('ts-lockers:client:DeleteLocker', { data = v, id = v.lockerid })
+            end
+        end
+        if not exist then
+            ESX.ShowNotification("You don't have a locker")
+        end
+    else
+        ESX.ShowNotification("You don't have a locker")
+    end
 end)
 
 RegisterNetEvent('ts-lockers:client:ChangePassword', function(info)
-    	local data = info.data
-    	local id = data.lockerid
-    	local input = lib.inputDialog('TS Lockers', {{ type = "input", label = "Locker Password", password = true, icon = 'lock' }})
-   	if input and input[1] then
-		TriggerServerEvent('ts-lockers:server:ChangePass', id, input[1])
-	end
+    local data = info.data
+    local id = data.lockerid
+    local input = lib.inputDialog('TS Lockers',
+        { { type = "input", label = "Locker Password", password = true, icon = 'lock' } })
+    if input and input[1] then
+        TriggerServerEvent('ts-lockers:server:ChangePass', id, input[1])
+    end
 end)
 
 RegisterNetEvent('ts-lockers:client:DeleteLocker', function(info)
     local data = info.data
     local id = info.id
-	lib.registerContext({
+    lib.registerContext({
         id = 'delete_locker_confirmation',
         title = 'Delete Locker',
-		menu = 'locker_menu',
+        menu = 'locker_menu',
         options = {
             ['Confirm'] = {
                 description = 'Confirm Deletion of Your Locker',
@@ -153,12 +157,12 @@ RegisterNetEvent('ts-lockers:client:DeleteLocker', function(info)
                 serverEvent = 'ts-lockers:server:DeleteLocker',
                 args = id
             },
-			['Cancel'] = {
+            ['Cancel'] = {
                 description = 'Cancel Deletion of Your Locker',
                 arrow = true,
                 menu = 'locker_menu'
             }
-			
+
         }
     })
     lib.showContext('delete_locker_confirmation')
@@ -168,35 +172,42 @@ RegisterNetEvent('ts-lockers:OpenSelfLocker', function(info)
     local Ply = ESX.GetPlayerData()
     local lockers = info.arg
     local branch = info.branch
-    for k,v in pairs(lockers) do
-        if Ply.identifier == v.owner then
-            exports.ox_inventory:setStashTarget(v.lockerid, nil)
-            ExecuteCommand('inv2')
-            exports.ox_inventory:setStashTarget(nil)
+    if lockers then
+        local exist = false
+        for k, v in pairs(lockers) do
+            if Ply.identifier == v.owner then
+                exist = true
+                exports.ox_inventory:openInventory('stash', v.lockerid)
+            end
         end
+        if not exist then
+            ESX.ShowNotification("You don't have a locker")
+        end
+    else
+        ESX.ShowNotification("You don't have a locker")
     end
 end)
 
 RegisterNetEvent('ts-lockers:client:OpenLocker', function(info)
-    	local data = info
-	local input = lib.inputDialog('TS Lockers', {{ type = "input", label = "Locker Password", password = true, icon = 'lock' }})
-	if input and input[1] then
-		if tostring(input[1]) == tostring(data.password) then
-                	exports.ox_inventory:setStashTarget(data.lockerid, nil)
-                	ExecuteCommand('inv2')
-                	exports.ox_inventory:setStashTarget(nil)
-            	else
-                	ESX.ShowNotification("Wrong Password")
-            	end
-	end
+    local data = info
+    local input = lib.inputDialog('TS Lockers',
+        { { type = "input", label = "Locker Password", password = true, icon = 'lock' } })
+    if input and input[1] then
+        if tostring(input[1]) == tostring(data.password) then
+            exports.ox_inventory:openInventory('stash', data.lockerid)
+        else
+            ESX.ShowNotification("Wrong Password")
+        end
+    end
 end)
 
 RegisterNetEvent("ts-lockers:CreateLocker", function(data)
-    	local area = data.branch
-	local input = lib.inputDialog('TS Lockers - Create Password', {{ type = "input", label = "Locker Password", password = true, icon = 'lock' }})
-	if input and input[1] then
-		TriggerServerEvent("ts-lockers:server:CreateLocker", input[1], area)
-	end
+    local area = data.branch
+    local input = lib.inputDialog('TS Lockers - Create Password',
+        { { type = "input", label = "Locker Password", password = true, icon = 'lock' } })
+    if input and input[1] then
+        TriggerServerEvent("ts-lockers:server:CreateLocker", input[1], area)
+    end
 end)
 
 function DrawText3Ds(x, y, z, text)
