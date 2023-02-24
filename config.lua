af466193bf0ce186e8fedbe2405cd50f049747b3
Tony Stark <76168122+tony-stark-17@users.noreply.github.com>
2023-02-24 13:37:39 +0530

    Update config.lua

--- config.lua
+++ config.lua
@@ -7,10 +7,12 @@ Config.MaxSlots = 50
 Config.MaxWeight = 5000000
 Config.LockerZone = {
     Legion2 = {
-        coords = vector3(-2031.3180, -464.9169, 10.6040)
+        coords = vector3(-2031.3180, -464.9169, 10.6040),
+        DrawDistance = 5
     },
     Legion = {
-        coords = vector3(-275.2015, -917.0491, 30.2163)
+        coords = vector3(-275.2015, -917.0491, 30.2163),
+        DrawDistance = 5
     },
 }
 
