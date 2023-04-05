bb5f4dd087bfdea16a6079bb6ac76da9e7a0ba69
Tony Stark <z.zedaniam@gmail.com>
2023-04-05 22:24:47 +0530

    Revert "xd"
    This reverts commit c9866f5a599959c2a3df7338697157094145df8a.

--- config.lua
+++ config.lua
@@ -1,9 +1,9 @@
 Config = {}
 
-Config.QBInventory = false --Set this to true if you're using the qbcore inventory, or false otherwise 
-Config.OXInventory = true  --Set this to true if you're using the ox_inventory script found at https://github.com/overextended/ox_inventory, or false otherwise
-Config.ChezzaInv = false  -- Set this to true if you're using the Chezza inventory system, or false otherwise
-Config.MaxSlots = 50  
+Config.QBInventory = false
+Config.OXInventory = true
+Config.ChezzaInv = false
+Config.MaxSlots = 50
 Config.MaxWeight = 5000000
 Config.LockerZone = {
     Legion2 = {
